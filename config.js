52d085e1106585d909a28bbc7ec9ced897933c42
Shivam Anand <45403712+shivamsancc@users.noreply.github.com>
2019-11-10 16:50:36 +0530

    first Commit

---
+++ config.js
@@ -0,0 +1,19 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+};
+CKEDITOR.on( 'instanceReady', function( evt ) {
+	evt.editor.dataProcessor.htmlFilter.addRules( {
+	  elements: {
+		img: function(el) {
+		  el.addClass('img-responsive');
+		}
+	  }
+	});
+  });
